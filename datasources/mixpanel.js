f3dcf4b1a72f6a13b30c0340d71edd8ce56fc6e2
Norman Joyner <norman.joyner@gmail.com>
2016-02-14 01:23:27 -0500

    use moment-timezone to do proper timezone change

--- datasources/mixpanel.js
+++ datasources/mixpanel.js
@@ -1,6 +1,6 @@
 var _ = require("lodash");
 var util = require("util");
-var moment = require("moment");
+var moment = require("moment-timezone");
 var MixpanelExport = require('mixpanel-data-export');
 var Datasource = require([__dirname, "..", "lib", "datasource"].join("/"));
 
@@ -12,6 +12,8 @@ function Mixpanel(options){
         api_key: this.options.api_key,
         api_secret: this.options.api_secret
     });
+
+    moment.tz.add("America/Los_Angeles|PST PDT|80 70|0101|1Lzm0 1zb0 Op0");
 }
 
 util.inherits(Mixpanel, Datasource);
@@ -25,7 +27,8 @@ Mixpanel.prototype.get_metrics = function(fn){
     }, function(response){
         var metrics = [];
         if(response && response.data && response.data.values){
-            var now = moment(new Date().setSeconds(0) - (1000 * 60 * 60 * 3) - (60 * 1000));
+            var now = moment(new Date().setSeconds(0) - (61 * 1000));
+            now.tz("America/Los_Angeles");
             var timestamp = now.format("YYYY-MM-DD HH:mm:00");
             _.each(response.data.values, function(series, key){
                 var metric = {};
